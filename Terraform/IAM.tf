4542d82c70b403e6ec5c0fb0d5218bd693ec7bfc
Ajinkya S Khandave <52904749+ajinkya48765@users.noreply.github.com>
2020-06-13 02:23:11 +0530

    Update IAM.tf

--- Terraform/IAM.tf
+++ Terraform/IAM.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_role_policy" "s3_policy" {
   name = "s3_policy"
-  role = aws_iam_role.s3_role.id
+  role = "${aws_iam_role.s3_role.id}"
 
   policy = "${file("s3_policy.json")}"
 }
