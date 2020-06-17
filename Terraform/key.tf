100330746ff95b9e1c54183a0faadd3515ec0778
Ajinkya S Khandave <52904749+ajinkya48765@users.noreply.github.com>
2020-06-17 23:41:48 +0530

    Update key.tf

--- Terraform/key.tf
+++ Terraform/key.tf
@@ -1,4 +1,8 @@
+resource "tls_private_key" "keygen" {
+  algorithm   = "RSA"
+}
+
 resource "aws_key_pair" "sshkey111" {
   key_name   = "sshkey111"
-  public_key = "${file("key.txt")}"
+  public_key = "${tls_private_key.keygen.public_key_openssh}"
 }
